3a62da445a635ecbef057c892ad84a81b2455fe8
wanghuamin <18219116508@163.com>
2022-08-15 11:17:55 +0800

    清理没用文件，配置项目编码统一风格

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,47 @@
+/**
+ *
+ * 规则说明见 https://cn.eslint.org/docs/rules/
+ * 规则值
+ * "off" 或 0 - 关闭规则
+ * "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出)
+ * "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
+ *
+ * 配置：取消组件名称校验；强制使用单引号；强制不使用分号结尾
+ */
 module.exports = {
+  // 用来告诉eslint找当前配置文件不能往父级查找
   root: true,
+
+  // 预定义的全局变量，这里是浏览器环境
   env: {
-    node: true
+    node: true,
+    es6: true,
+    commonjs: true,
+    browser: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser' // 解析器，默认使用 Espree
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: ['warn', 'single'], // 引号类型 `` '' ''  强制使用单引号
+    semi: ['error', 'always'], // 语句强制分号结尾
+    'semi-spacing': ['off', { before: false, after: true }], // 分号前后空格
+    'arrow-parens': 'off', // 箭头函数用小括号括起来
+    'generator-star-spacing': 'off', // 生成器函数*的前后空格
+    'space-before-function-paren': ['off', 'always'], // 函数定义时括号前面要不要有空格
+    'eol-last': 'off', // 文件以单一的换行符结束
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'all',
+        ignoreReadBeforeAssign: false
+      }
+    ], // 首选const
+    'guard-for-in': 'off' // for in循环要用if语句过滤
+    // 'vue/multi-word-component-names': 0, // 取消组件名称校验
   },
   overrides: [
     {
@@ -25,4 +54,4 @@ module.exports = {
       }
     }
   ]
-}
+};
--- README.md
+++ README.md
@@ -27,3 +27,23 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 配置说明
+```
+vue 脚手架构建项目
+```
+```
+1. .editorconfig // 字符集配置，缩进等配置
+  不同的开发人员，不同的编辑器，有不同的编码风格，而EditorConfig就是用来协同团队开发人员之间的代码的风格及样式规范化的一个工具，而.editorconfig正是它的默认配置文件。
+
+  在vsCode编辑器安装插件：EditorConfig for VS Code
+2. .eslintrc.js
+
+3.文件保存自动将‘单引号’改为“双引号”，自动给末尾添加逗号，自动给每一项 结尾添加逗号，导致eslint报警告，只需要一步解决：尤其‘末尾禁止添加逗号’
+在项目根目录新建.prettierrc 文件，添加对象：
+{
+  "semi": false, // 去掉结尾的分号
+  "singleQuote": true, // 单引号替代双引号
+  "trailingComma": "none"// 末尾禁止添加逗号
+}
+```
